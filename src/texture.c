1db58d6a8b272a48c9b440f5ac76b2e70175ea0c
Nikos Plugachev <nikos@plugachev.com>
2022-11-09 09:38:19 -0500

    Made textures clamp to border

--- src/texture.c
+++ src/texture.c
@@ -5,8 +5,8 @@
 void lms_initTexture(lms_texture* tex) {
     glGenTextures(1, &tex->texture);
     glBindTexture(GL_TEXTURE_2D, tex->texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
